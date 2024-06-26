3fa35321217ef57352b5ce53c9c6608fd4948a6a
hyeji111544 <hyeji111544@gmail.com>
2024-06-27 02:07:15 +0900

    백준 11654 추가

---
+++ src/step1/Main1_11654.java
@@ -0,0 +1,14 @@
+package step1;
+
+import java.util.Scanner;
+
+public class Main1_11654 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		byte a = (byte) sc.nextLine().charAt(0);
+		
+		System.out.println(a);
+	}
+
+}
